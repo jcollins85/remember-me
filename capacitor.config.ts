cc174a2089fffdf69dd72b3373743dca9e7352ba
Julian Collins <Juelz@macbook-pro.home>
2025-12-18 00:16:31 -0500

    chore: initialize Capacitor for MetHere

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eraone.methere',
+  appName: 'MetHere',
+  webDir: 'dist'
+};
+
+export default config;
